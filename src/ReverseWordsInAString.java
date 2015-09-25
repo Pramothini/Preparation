fb0c660a83c5203edc97d9c53a6936fa0b961216
Pramothini <pramothinidk@gmail.com>
2015-09-24 21:24:11 -0700

    reverse words in a string entered via console
    Eg: how are you - uoy era woh

---
+++ src/ReverseWordsInAString.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+/**
+ * reverse the words in a string entered via console
+ * Eg: how are you - uoy era woh
+ * @author pramothinidk
+ *
+ */
+public class ReverseWordsInAString {
+	public void reverseWords(String input){
+		String[] original = input.split(" ");
+		StringBuilder sb = new StringBuilder();
+		for(int i = (original.length - 1) ; i >= 0 ; i--){
+			sb.append(reverseCharacters(original[i])).append(" ");
+		}
+		System.out.println("the reversed string - "+ sb);
+		
+		//output:
+		//how are you
+		//the reversed string - uoy era woh 
+	}
+	
+	public StringBuilder reverseCharacters(String input){
+		StringBuilder sb = new StringBuilder();
+		for(int i=input.length() - 1 ; i>=0 ; i--){
+			sb.append(input.charAt(i));
+		}
+		return sb;
+	}
+
+	public static void main(String args[]){
+		ReverseWordsInAString rstr = new ReverseWordsInAString();
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		rstr.reverseWords(input);
+	}
+
+}
