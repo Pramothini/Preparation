9ac931a26407b9fb7040aa6af74254c513de8b7e
Pramothini <pramothinidk@gmail.com>
2015-09-29 20:23:56 -0700

    added comments

--- src/hackerRank/string_hackerrank/AlternateChars.java
+++ src/hackerRank/string_hackerrank/AlternateChars.java
@@ -3,8 +3,25 @@ package hackerRank.string_hackerrank;
 import java.util.Scanner;
 
 /**
+ * https://www.hackerrank.com/challenges/alternating-characters/copy-from/14092702
  * Shashank likes strings in which consecutive characters are different. For example, he likes ABABA, while he doesn't like ABAA. Given a string containing characters A and B only, he wants to change it into a string he likes. To do this, he is allowed to delete the characters in the string.
  * Your task is to find the minimum number of required deletions.
+ * 
+ * sample input:
+ *  5
+	AAAA
+	BBBBB
+	ABABABAB
+	BABABA
+	AAABBB
+
+ * sample output:
+ *  3
+	4
+	0
+	0
+	4
+	
  * @author pramothinidk
  *
  */
