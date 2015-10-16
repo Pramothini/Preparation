73316783e44e9c82a2186f743d7ab64ae2bcc1c0
Pramothini <pramothinidk@gmail.com>
2015-10-15 23:14:33 -0700

    corrected indentation

--- src/leetcode/AddDigits.java
+++ src/leetcode/AddDigits.java
@@ -10,29 +10,29 @@ Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one
  *
  */
 public class AddDigits {
-	
+
 	public int addDigits(int num) {
-        int result = num;
-        while(result/10 >= 1){
-            result = getResult(result);
-        }
-        return result;
-        
-    }
-    
-    public int getResult(int num){
-        int result = 0;
-        while(num > 0){
-            result += num % 10;
-            num = num / 10;
-        }
-        return result;
-    }
-    
-    public static void main(String args[]){
-    	AddDigits a = new AddDigits();
-    	System.out.println(a.addDigits(199));
-    	System.out.println(a.addDigits(38));
-    }
+		int result = num;
+		while(result/10 >= 1){
+			result = getResult(result);
+		}
+		return result;
+
+	}
+
+	public int getResult(int num){
+		int result = 0;
+		while(num > 0){
+			result += num % 10;
+			num = num / 10;
+		}
+		return result;
+	}
+
+	public static void main(String args[]){
+		AddDigits a = new AddDigits();
+		System.out.println(a.addDigits(199));
+		System.out.println(a.addDigits(38));
+	}
 
 }
