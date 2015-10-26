d5ae76064004d4eb4ce3d9ed853a14772587a3e4
Pramothini <pramothinidk@gmail.com>
2015-10-26 01:09:30 -0700

    fib series improved

--- src/FibonacciSeries.java
+++ src/FibonacciSeries.java
@@ -6,24 +6,41 @@
  */
 public class FibonacciSeries {
 	int[] fibonacciSeries(int n){
-		int fibSeries[] = new int[n];
-		int a,b;
-		fibSeries[0] = a = 0;
-		fibSeries[1] = b = 1;
-		int i = 2;
-		while(i < n){
-			int c = a+b;
-			fibSeries[i] = c;
+//		int fibSeries[] = new int[n];
+//		int a,b;
+//		fibSeries[0] = a = 0;
+//		fibSeries[1] = b = 1;
+//		int i = 2;
+//		while(i < n){
+//			int c = a+b;
+//			fibSeries[i] = c;
+//			a = b;
+//			b = c;
+//			i++;
+//		}
+//		return fibSeries;
+		
+		
+		
+		
+		
+		
+		int[] fibseries = new int[n];
+		int a = 0,b = 1;
+		if(n>=1) fibseries[0] = a;
+		if(n>=2) fibseries[1] = b;
+		for(int i=2; i<n ; i++){
+			fibseries[i] = a+b;
 			a = b;
-			b = c;
-			i++;
+			b = fibseries[i];
 		}
-		return fibSeries;
+		return fibseries;
+		
 	}
 
 	public static void main(String args[]){
 		FibonacciSeries fs = new FibonacciSeries();
-		int[] fibSeriesOutput = fs.fibonacciSeries(10);
+		int[] fibSeriesOutput = fs.fibonacciSeries(2);
 		for(int i=0 ; i < fibSeriesOutput.length ; i++ ){
 			System.out.println(fibSeriesOutput[i]);
 		}
