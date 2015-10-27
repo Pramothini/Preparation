70f492b2c4406efd53447d60e8baa68cdf62734a
Pramothini <pramothinidk@gmail.com>
2015-10-27 10:13:08 -0700

    maxConsecutiveSum

---
+++ src/ProgramingPearls/MaxConsecutiveSum.java
@@ -0,0 +1,21 @@
+package ProgramingPearls;
+
+public class MaxConsecutiveSum {
+
+	public void getMaxSum(int[] input){
+		int maxsofar = 0;
+		int maxendinghere = 0;
+		for(int i=0;i<input.length;i++){
+			maxendinghere = Math.max(maxendinghere + input[i] , 0);
+			maxsofar = Math.max(maxendinghere, maxsofar);
+		}
+		System.out.println("max sum is "+maxsofar);
+	}
+	
+	public static void main(String args[]){
+		MaxConsecutiveSum ms = new MaxConsecutiveSum();
+		int[] input = {31,-41,59,26,-53,58,97,-93,-23,84};
+		//int[] input = {31,-41,59,26,-53,58,97,-93,-23,84};
+		ms.getMaxSum(input);
+	}
+}
