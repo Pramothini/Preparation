f865168607f02c8be6c413412831adc265574eb0
Pramothini <pramothinidk@gmail.com>
2015-11-09 11:50:57 -0800

    Changed the folder location

--- src/BitProblems/SingleNumber3.java
+++ src/BitProblems/SingleNumber3.java
@@ -1,5 +1,13 @@
-package leetcode;
+package BitProblems;
+/**
+ * Given an array of numbers nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once.
 
+For example:
+
+Given nums = [1, 2, 1, 3, 2, 5], return [3, 5].
+ * @author pramothinidk
+ *
+ */
 public class SingleNumber3 {
 	public int[] singleNumber(int[] nums) {
         int exoredresult = 0;
