c4317ef030433b4986c90eaceccf4fa65e2ed8ed
Pramothini <pramothinidk@gmail.com>
2015-11-10 18:35:12 -0800

    Add Binary Strings
    Given two binary strings, return their sum (also a binary string

--- src/leetcode/AddDigits.java
+++ src/leetcode/AddDigits.java
@@ -10,6 +10,39 @@ Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one
  *
  */
 public class AddDigits {
+	
+	/**
+	 * Given two binary strings, return their sum (also a binary string).
+
+For example,
+a = "11"
+b = "1"
+Return "100".
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public String addBinary(String a, String b) {
+        StringBuilder res = new StringBuilder();
+        int x=0; int y=0; int carry=0;
+        while(a.length() != b.length()){
+            if(a.length() > b.length()){
+                b = "0"+b;
+            }
+            else{
+                a = "0"+a;
+            }
+        }
+        for(int i=a.length()-1; i>=0; i--){
+            x = Character.getNumericValue(a.charAt(i));
+            y = Character.getNumericValue(b.charAt(i));
+            res.append((x+y+carry)%2);
+            carry = (x+y+carry)/2;
+        }
+        
+        if(carry == 1) res.append(1);
+        return res.reverse().toString();
+    }
 
 	public int addDigits(int num) {
 		int result = num;
@@ -33,6 +66,7 @@ public class AddDigits {
 		AddDigits a = new AddDigits();
 		System.out.println(a.addDigits(199));
 		System.out.println(a.addDigits(38));
+		System.out.println(a.addBinary("11","1"));
 	}
 
 }
